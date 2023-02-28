7358e3a645be66c81482081e28b17d1480e2ec44
Santij25 <santijurado25@gmail.com>
2023-02-28 15:43:43 -0300

    css terminado

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getRecipeId } from "../../redux/Actions";
 import style from "./Detail.module.css";
+import logo from "../../Media/food-restaurant.png";
 
 const Detail = () => {
   const { id } = useParams();
@@ -14,28 +15,63 @@ const Detail = () => {
   }, [dispatch, id]);
 
   return (
-    <div className={style.container}>
-      <h6>name: {recipe.name}</h6>
-      <img src={recipe.imagen} alt="receta" />
-      <h6 dangerouslySetInnerHTML={{__html: recipe.resumenDelPlato}}></h6>
-      <h6>healthScore: {recipe.healthScore}</h6>
-      <h6>diets:{recipe.diets}</h6>
-      <ul>
-        steps:
-        {recipe.pasoAPaso ? (
-          typeof recipe.pasoAPaso[0] === "object" ? (
-            recipe.pasoAPaso.map((r) => {
-              return <li>{r.number + " " + r.step}</li>;
-            })
-          ) : (
-            recipe.pasoAPaso.map((r) => {
-              return <li>{r}</li>;
-            })
-          )
-        ) : (
-          <p>No hay pasos 😔</p>
-        )}
-      </ul>
+    <div className={style.padre}>
+      <div className={style.container}>
+        <div className={style.primero}>
+          <div className={style.segundo}>
+            <div className={style.name}>
+              <h1>Name:</h1>
+              <p>{recipe.name}</p>
+            </div>
+            <div className={style.score}>
+              <h1>HealthScore:</h1>
+              <p>❤️{recipe.healthScore}</p>
+            </div>
+            <div className={style.diets}>
+              <h1>Diets:</h1>
+              <div className={style.dietsCh}>
+                {recipe.diets ? (
+                  recipe.diets.map((e) => (
+                    <span>
+                      <img src={logo} alt="" />
+                      {e}
+                    </span>
+                  ))
+                ) : (
+                  <span></span>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className={style.img}>
+            <img src={recipe.imagen} alt="receta" />
+          </div>
+        </div>
+        <div className={style.abajo}>
+          <div className={style.summary}>
+            <h1>Summary:</h1>
+            <p dangerouslySetInnerHTML={{ __html: recipe.resumenDelPlato }}></p>
+          </div>
+          <div className={style.steps}>
+            <h1>Steps:</h1>
+            <ul>
+              {recipe.pasoAPaso ? (
+                typeof recipe.pasoAPaso[0] === "object" ? (
+                  recipe.pasoAPaso.map((r) => {
+                    return <li>{r.number + " " + r.step}</li>;
+                  })
+                ) : (
+                  recipe.pasoAPaso.map((r) => {
+                    return <li>{r}</li>;
+                  })
+                )
+              ) : (
+                <p>No hay pasos 😔</p>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
