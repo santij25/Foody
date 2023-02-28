c43d245f2f46d9bebfa85324392587d634eda664
Santij25 <santijurado25@gmail.com>
2023-02-27 21:55:33 -0300

    cards terminadas

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,18 +1,21 @@
 import { Link } from "react-router-dom";
 import style from "./Card.module.css";
+import logo from "../../Media/food-restaurant.png"
 
 const Card = (props) => {
   // console.log(props.id);
   return (
     <div className={style.card}>
       <img src={props.imagen} alt="plato" className={style.img}/>
+      <div className={style.descripcion}>
       <Link to={`/Detail/${props.id}`} className={style.link}>
         <h1>{props.name}</h1>
       </Link>
       {/* <p>resumenDelPlato:{props.resumenDelPlato}</p> */}
       {/* <div className={style.barprog}> <p>{props.healthScore}%</p></div> */}
       <div className={style.diets}>
-      {props.diets.map(d => <span>☑{d}</span>)}
+      {props.diets.map(d => <span><img src={logo} alt="" />{d}</span>)}
+      </div>
       </div>
       {/* <p> pasoAPaso:{props.pasoAPaso}</p> */}
     </div>
